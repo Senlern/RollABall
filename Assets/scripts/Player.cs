026f68b7fdfc1f02f13f98c5c37b9d525c5e3407
zhangxinlin <429350061@qq.com>
2021-09-02 19:48:19 +0800

    新建项目

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody rd;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rd = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //rd.AddForce(new Vector3(1,0,0));
+        float h = Input.GetAxis("Horizontal");
+        Debug.Log(h);
     }
 }
